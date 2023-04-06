fa627323ee3d8bceace124327f097f674158256d
Jose Antonio Albértuz <josealbertuz@gmail.com>
2023-04-06 18:15:16 +0200

    feat: install react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,39 +1,45 @@
-import { ThemeProvider } from "next-themes";
-import type { AppProps } from "next/app";
-import { Inter, Inconsolata, Lora } from "next/font/google";
-import { darkTheme, generateGlobalStyles, globalCss } from "stitches.config";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from 'next-themes'
+import type { AppProps } from 'next/app'
+import { Inter, Inconsolata, Lora } from 'next/font/google'
+import { useRef } from 'react'
+import { darkTheme, generateGlobalStyles, globalCss } from 'stitches.config'
 
 const inter = Inter({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-  variable: "--inter-font",
-});
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--inter-font',
+})
 
 const inconsolata = Inconsolata({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-  variable: "--inconsolata-font",
-});
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--inconsolata-font',
+})
 
 const lora = Lora({
-  weight: ["400", "700"],
-  style: ["italic", "normal"],
-  subsets: ["latin"],
-  variable: "--lora-font",
-});
+  weight: ['400', '700'],
+  style: ['italic', 'normal'],
+  subsets: ['latin'],
+  variable: '--lora-font',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = useRef(new QueryClient())
+
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      value={{
-        dark: darkTheme.className,
-        light: "light",
-      }}
-    >
-      {generateGlobalStyles({ lora, inter, inconsolata })}
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+    <QueryClientProvider client={queryClient.current}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        value={{
+          dark: darkTheme.className,
+          light: 'light',
+        }}
+      >
+        {generateGlobalStyles({ lora, inter, inconsolata })}
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </QueryClientProvider>
+  )
 }
